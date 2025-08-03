89507d16c52340125f32b130edbc00924a0bc039
melih agraz <37498443+melihagraz@users.noreply.github.com>
2025-08-04 00:18:59 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HFpEFClinicalDecisionTools from './HFpEFClinicalDecisionTools';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HFpEFClinicalDecisionTools />
+  </React.StrictMode>
+);
